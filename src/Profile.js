78abbd43f5ba67c2c0d164db9464178e9d7750ba
Anna McCreery <amccreery@madisoncollege.edu>
2023-04-01 09:52:26 -0500

    added Profile code

---
+++ src/Profile.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import { Auth } from 'aws-amplify';
+import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";  
+import Container from "./Container";
+
+const Profile = () => {
+    useEffect(() => {
+        checkUser()
+    }, [])
+
+    const [user, setUser] = useState({})
+
+    const checkUser = asyn() => {
+        try {
+            const data = await Auth.currentUserPoolUser()
+            const userInfo = {username: data.username, ...data.attributes}
+            setUser(userInfo)
+        } catch (err) { console.log('error: ', err)}
+    }
+    return (
+        <Container>
+            <h1>Profile</h1>
+            <h2>UsernName: {user.username}</h2>
+            <h3>Email: {user.email}</h3>
+            <h4>Phone: {user.phone_number}</h4>
+            <AmplifySignOut />
+        </Container>
+    );
+}
+
+export default withAuthenticator(Profile)
