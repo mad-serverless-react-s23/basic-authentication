8b295a69091561494009e2f574ea79293e5d9b2e
Anna McCreery <amccreery@madisoncollege.edu>
2023-04-06 15:35:57 -0500

    debugging Nav

--- src/Nav.js
+++ src/Nav.js
@@ -11,24 +11,24 @@ const Nav = (props) => {
     const { current } = props
     return (
         <div>
-            <Menu selectedKeys={[current]} mode="horizontal">
-                <Menu.Item key='home'>
-                    <Link to={'/'}>
-                        <HomeOutlined />Home
-                    </Link>
-                </Menu.Item>
-                <Menu.Item key='profile'>
-                    <Link to={'/profile'}>
-                        <ProfileOutlined />Profile
-                    </Link>
-                </Menu.Item>
-                <Menu.Item key='protected'>
-                    <Link to={'/protected'}>
-                        <FileProtectOutlined />Protected
-                    </Link>
-                </Menu.Item>
-            </Menu>
-        </div>
+        <Menu selectedKeys={[current]} mode="horizontal">
+          <Menu.Item key='home'>
+            <Link to={`/`}>
+              <HomeOutlined />Home
+            </Link>
+          </Menu.Item>
+          <Menu.Item key='profile'>
+            <Link to='/profile'>
+            <ProfileOutlined />Profile
+            </Link>
+          </Menu.Item>
+          <Menu.Item key='protected'>
+            <Link to='/protected'>
+              <FileProtectOutlined />Protected
+            </Link>
+          </Menu.Item>
+        </Menu>
+      </div>
     )
 };
 
