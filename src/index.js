764c04410c61e071e2fda241b2a1a9f46201ad34
Anna McCreery <amccreery@madisoncollege.edu>
2023-04-06 14:47:20 -0500

    deugging- r
    replaced import 'antd/dist/antd.css' with import 'antd/dist/reset.css';
    
    and added {} around Amplify

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Router from './Router';
-import 'antd/dist/antd.css';
+import 'antd/dist/reset.css';
 
-import Amplify from 'aws-amplify'
+import { Amplify } from 'aws-amplify'
 import config from './aws-exports'
 Amplify.configure(config)
 
 ReactDOM.render(<Router />, document.getElementById('root'));
-
